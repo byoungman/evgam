0bfabf8ea3057a7870bf8f9dd11031145f486125
byoungman <45686107+byoungman@users.noreply.github.com>
2021-06-29 10:21:11 +0100

    Update setup_and_fit.R

--- R/setup_and_fit.R
+++ R/setup_and_fit.R
@@ -549,27 +549,32 @@ likdata0$S <- diag(0, npar)
 likdata0$idpars <- seq_len(npar)
 
 if (is.null(inits)) {
-  if (npar == 1) 
-    inits <- 2
-  if (npar == 2) {
-    if (family == "ald") {
-      inits <- c(quantile(likdata0$y[,1], likdata0$tau), log(sd(likdata0$y[,1])))
-    } else {
-      inits <- c(log(mean(likdata$y[,1])), .05)
-      if (family == "transxigpd") 
-        inits[2] <- .9
+  if (family == "egpd") {
+    inits <- numeric(npar)
+    inits[1:2] <- c(log(mean(likdata$y[,1])), .05)
+  } else {
+    if (npar == 1) 
+      inits <- 2
+    if (npar == 2) {
+      if (family == "ald") {
+        inits <- c(quantile(likdata0$y[,1], likdata0$tau), log(sd(likdata0$y[,1])))
+      } else {
+        inits <- c(log(mean(likdata$y[,1])), .05)
+        if (family == "transxigpd") 
+          inits[2] <- .9
+      }
     }
-  }
-  if (npar %in% 3:4) {
-    inits <- c(sqrt(6) * sd(likdata0$y[,1]) / pi, .05)
-    inits <- c(mean(likdata0$y[,1]) - .5772 * inits[1], log(inits[1]), inits[2])
-    if (npar == 4) 
-      inits <- c(inits, 1)
-  }
-  if (npar == 6) {
-    inits <- c(sqrt(6) * sd(likdata0$y[,1]) / pi, .05)
-    inits <- c(mean(likdata0$y[,1]) - .5772 * inits[1], log(inits[1]), inits[2])
-    inits <- c(inits, 0, 0, 1)
+    if (npar %in% 3:4) {
+      inits <- c(sqrt(6) * sd(likdata0$y[,1]) / pi, .05)
+      inits <- c(mean(likdata0$y[,1]) - .5772 * inits[1], log(inits[1]), inits[2])
+      if (npar == 4) 
+        inits <- c(inits, 1)
+    }
+    if (npar == 6) {
+      inits <- c(sqrt(6) * sd(likdata0$y[,1]) / pi, .05)
+      inits <- c(mean(likdata0$y[,1]) - .5772 * inits[1], log(inits[1]), inits[2])
+      inits <- c(inits, 0, 0, 1)
+    }
   }
   likdata0$CH <- diag(length(inits))
   likdata0$compmode <- numeric(length(inits))
